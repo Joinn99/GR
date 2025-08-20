20798cceb036f9106b4715812540b5d66529fa3d
Joinn99 <Joinnnor@outlook.com>
2025-08-20 23:08:02 +0800

    Tokenizer

--- processor/item_tokenize.py
+++ processor/item_tokenize.py
@@ -4,6 +4,16 @@ import numpy as np
 import pandas as pd
 from logger import get_logger, log_with_color
 
+def get_neighbor_indices(codes, neighbor_indices):
+    new_codes = []
+    n = neighbor_indices.shape[0]
+    for code in codes:
+        cur = code
+        i = 0
+        while i < n and neighbor_indices[cur, i] in new_codes:
+            i += 1
+        new_codes.append(neighbor_indices[cur, i % n])
+    return np.array(new_codes)
 
 if __name__ == "__main__":
     import argparse
@@ -37,7 +47,7 @@ if __name__ == "__main__":
         torch.random.manual_seed(0)
         residual_vq = ResidualVQ(
             dim = 4096,
-            num_quantizers = 3,      # specify number of quantizers
+            num_quantizers = 4,      # specify number of quantizers
             codebook_size = 256,    # codebook size
             kmeans_init = True,   # set to True
             kmeans_iters = 10,     # number of kmeans iterations to calculate the centroids for the codebook on init
@@ -50,13 +60,21 @@ if __name__ == "__main__":
         log_with_color(logger, "ERROR", f"Item tokenization failed: {e}", "red")
         raise
 
-    index = pd.DataFrame(indices.to("cpu").numpy())
+    index = pd.DataFrame(indices.to("cpu").numpy()).reset_index()
+    code_embed = residual_vq.state_dict()["layers.3._codebook.embed"] # Num_Head * Num_Code * Codebook_Size
+    # Calculate L2 distances between all code embeddings
+    distances = torch.cdist(code_embed.view(-1, code_embed.shape[-1]), code_embed.view(-1, code_embed.shape[-1]), p=2)
+    neighbor_indices = torch.topk(distances, k=distances.shape[1], dim=1, largest=False).indices.cpu().numpy()
 
-    last_id = index.groupby(list(index.columns)).cumcount()
-    log_with_color(logger, "INFO", f"Saving index to {index_path}, max cluster size: {last_id.max()}", "red")
-    index[len(index.columns)] = last_id
+    tmp_index = index.groupby(list(range(args.n_layers-1))).agg(list)
+    max_cluster_size = tmp_index.loc[:, args.n_layers-1].apply(len).max()
+    tmp_index.loc[:, args.n_layers-1] = tmp_index.loc[:, args.n_layers-1].apply(lambda x: get_neighbor_indices(x, neighbor_indices))
+    index = tmp_index.reset_index().explode(["index", args.n_layers-1]).set_index("index").sort_index()
+
+    log_with_color(logger, "INFO", f"Saving index to {index_path}, max cluster size: {max_cluster_size}", "red")
+
     index = index.rename(columns={c: f"ID_{chr(c+97)}" for c in range(len(index.columns))})
-    index.loc[:, "sem_id"] = index.apply(lambda x: "".join([f"<{chr(c+97)}_{x[c]}>" for c in range(len(index.columns))]), axis=1)
+    index.loc[:, "sem_id"] = index.apply(lambda x: "".join([f"<{chr(c+97)}_{x.iloc[c]}>" for c in range(args.n_layers)]), axis=1)
     index.to_json(index_path, orient="records", lines=True)
     log_with_color(logger, "INFO", f"Saved index to {index_path}", "cyan")
 
